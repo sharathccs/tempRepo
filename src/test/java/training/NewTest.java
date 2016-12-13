f2e39f7edc8f93b721bce991f52e20aec8aa11c3
sharathccs <sharath84@gmail.com>
2016-12-13 11:24:11 +0530

    added a new test

---
+++ src/test/java/training/NewTest.java
@@ -0,0 +1,16 @@
+package training;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+  @Test
+  public void f() {
+	  
+	  System.out.println("ljlj");
+	  Assert.assertEquals("test1","test1");
+	  
+	  
+  }
+}
